4a351a2a750557d544b9b33d9c56f3763230fa32
Will Smith <smithywill@gmail.com>
2015-07-22 22:06:25 +0100

    Added namespace to urls so django knows which app view to take if two apps share same view name

--- playground/playground/urls.py
+++ playground/playground/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$','testapp.views.home', name='home'),
-    url(r'^polls/',include('polls.urls'))
+    url(r'^polls/',include('polls.urls', namespace="polls"))
 ]

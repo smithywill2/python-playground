a943cf4292f728a72c377c3c487ec6553948d975
Will Smith <smithywill@gmail.com>
2015-07-22 22:00:32 +0100

    Created my own foo endpoint and raised a 404 for details endpoint

--- playground/polls/urls.py
+++ playground/polls/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^foo/$',views.foo,name='foo')
 ]

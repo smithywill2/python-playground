42233912250086438e8c2bf547e866765ea860cc
Will Smith <smithywill@gmail.com>
2015-07-27 22:21:56 +0100

    Finished testin module

--- playground/polls/tests.py
+++ playground/polls/tests.py
@@ -20,6 +20,31 @@ def create_question(question_text, days):
                                    pub_date=time)
 
 
+class QuestionIndexDetailtests(TestCase):
+
+    def test_detail_view_with_a_future_question(self):
+        """
+        The detail view of a question should return 404 if pub date is in future
+        :return:
+        """
+
+        future_question = create_question("Some future q",5)
+        response=self.client.get(reverse('polls:detail', args=(future_question.id,)))
+        self.assertEqual(response.status_code,404)
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past should
+        display the question's text.
+        """
+        past_question = create_question(question_text='Past Question.',
+                                        days=-5)
+        response = self.client.get(reverse('polls:detail',
+                                   args=(past_question.id,)))
+        self.assertContains(response, past_question.question_text,
+                            status_code=200)
+
+
 class QuestionMethodTests(TestCase):
 
     def test_index_view_with_no_questions(self):
